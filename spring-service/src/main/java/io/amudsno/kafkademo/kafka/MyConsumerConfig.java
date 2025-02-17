db33d803143e91a5717123138994feae443e9354
gns3922 <amund-stensrud.normann@teliacompany.com>
2025-02-17 18:39:37 +0100

    add consumers deserializing strings and byte arrays

--- spring-service/src/main/java/io/amudsno/kafkademo/kafka/MyConsumerConfig.java
+++ spring-service/src/main/java/io/amudsno/kafkademo/kafka/MyConsumerConfig.java
@@ -1,16 +1,16 @@
 package io.amudsno.kafkademo.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class MyConsumerConfig {
@@ -20,12 +20,68 @@ public class MyConsumerConfig {
     @Value("${my-properties.topic-name}")
     private String topicName;
 
+    /*
+    // If only one kind of deserialization is needed, injecting this bean allows Spring Boot Kafka to
+    // autoconfigure the required ListenerContainerFactory and ConsumerFactory
     @Bean
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>(kafkaProperties.buildConsumerProperties());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return props;
+    }
+     */
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        final JsonDeserializer<Object> jsonDeserializer = new JsonDeserializer<>();
+        jsonDeserializer.addTrustedPackages("*"); // Trust all packages to be deserialized
+        return new DefaultKafkaConsumerFactory<>(
+                kafkaProperties.buildConsumerProperties(),
+                new StringDeserializer(),
+                jsonDeserializer);
+    }
+
+    @Bean
+    // The listener container manages consumers and polling the kafka cluster
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> stringConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(
+                kafkaProperties.buildConsumerProperties(),
+                new StringDeserializer(),
+                new StringDeserializer());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerStringContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(stringConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    // Kafka messages are transported as byte arrays
+    public ConsumerFactory<String, byte[]> byteArrayConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(
+                kafkaProperties.buildConsumerProperties(),
+                new StringDeserializer(),
+                new ByteArrayDeserializer());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, byte[]> kafkaListenerByteArrayContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, byte[]> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(byteArrayConsumerFactory());
+        return factory;
     }
 
 }
--- spring-service/src/main/java/io/amudsno/kafkademo/service/MyConsumerService.java
+++ spring-service/src/main/java/io/amudsno/kafkademo/service/MyConsumerService.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +13,38 @@ public class MyConsumerService {
 
     private static final Logger logger = LoggerFactory.getLogger(MyConsumerService.class);
 
+    /*
+    // Bare-bones listener, using the default consumer configuration for deserialization
     @KafkaListener(topics = "${my-properties.topic-name}")
     public void listen(ConsumerRecord<String, MyEvent> record) {
         logger.info("Received: {}", record);
     }
+     */
 
+    @KafkaListener(
+            topics = "${my-properties.topic-name}",
+            clientIdPrefix = "json",
+            containerFactory = "kafkaListenerContainerFactory" // Inject the bean we defined in our configuration
+    )
+    public void listenAsJsonObject(ConsumerRecord<String, Object> record, @Payload MyEvent payload) {
+        logger.info("Logger [JSON] received key: {} | payload: {} | record: {}", record.key(), payload, record);
+    }
+
+    @KafkaListener(
+            topics = "${my-properties.topic-name}",
+            clientIdPrefix = "string",
+            containerFactory = "kafkaListenerStringContainerFactory"
+    )
+    public void listenAsString(ConsumerRecord<String, String> record, @Payload String payload) {
+        logger.info("Logger [String] received key: {} | payload: {} | record: {}", record.key(), payload, record);
+    }
+
+    @KafkaListener(
+            topics = "${my-properties.topic-name}",
+            clientIdPrefix = "byte",
+            containerFactory = "kafkaListenerByteArrayContainerFactory"
+    )
+    public void listenAsByteArray(ConsumerRecord<String, byte[]> record, @Payload byte[] payload) {
+        logger.info("Logger [ByteArray] received key: {} | payload: {} | record: {}", record.key(), payload, record);
+    }
 }
